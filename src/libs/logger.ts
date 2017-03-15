10654744f2781f29dd94dce632e832c54fed3d41
Eric Lara <ericzon@gmail.com>
2017-03-15 12:59:04 +0100

    UPDATE logger to display mongo objects nice

--- src/libs/logger.ts
+++ src/libs/logger.ts
@@ -3,16 +3,30 @@
 import * as winston from "winston";
 
 const tsFormat = () => (new Date());
-const logger = new (winston.Logger)({
-  transports: [
-    new (winston.transports.Console)({
+const consoleTransport = new (winston.transports.Console)({
       colorize: true,
       prettyPrint: true,
       timestamp: tsFormat
-    })]
+    });
+
+
+const originalLog = consoleTransport.log;
+consoleTransport.log = function(level: any, msg: any, meta: any, callback: any) {
+  if(meta) {
+    if(meta instanceof Error) {
+      meta = meta.toString();
+    } else {
+      meta = JSON.parse(JSON.stringify(meta));
+    }
+  }
+  return originalLog.call(consoleTransport, level, msg, meta, callback);
+};
+
+const logger = new (winston.Logger)({
+  transports: [ consoleTransport ]
 });
 
-logger.level = (process.env.LOG_LEVEL || "info");
-console.log("logger.level: ",logger.level);
+logger.level = (process.env.LOG_LEVEL || "verbose");
+console.log("Logger level: ",logger.level);
 
 export default logger;
