c0dfa2de3c4522c5d67cc76272d317fe77bb0288
Eric Lara Amat y Leon <ericlara.amatyleon@altran.es>
2017-04-10 16:38:04 +0200

    ADD compression

--- src/server.ts
+++ src/server.ts
@@ -8,11 +8,13 @@ import { RoutesProvider } from './routes';
 import { ToolsController } from './controllers/tools.server.controller';
 import logger from './libs/logger';
 import config from './config/config';
+import * as compression from 'compression';
 
 const favicon = require('serve-favicons');
 const port = ToolsController.normalizePort( process.env.PORT || config.port );
 const app: express.Application = express();
 app.use(favicon(__dirname + '/../public/favicon.ico'));
+app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( { extended: false } ));
 app.use(cors());
